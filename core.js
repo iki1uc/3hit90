00bd8d2bf97f1bb05049b16106256687b240156e
iki1uc <entertrainer@web.de>
2026-07-03 23:30:42 +0200

    Update core.js

--- core.js
+++ core.js
@@ -1,1 +1,8 @@
-
+export function CORE(){
+  return {
+    CPU:  Math.floor(Math.random()*100),
+    RAM:  Math.floor(Math.random()*100),
+    GPU:  Math.floor(Math.random()*100),
+    ROM:  Math.floor(Math.random()*100)
+  };
+}
